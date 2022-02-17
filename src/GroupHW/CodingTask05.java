fae9a52c4cc061f12fcc912ab638f6f7a1bafe65
Michael <fallen0331@gmail.com>
2022-02-16 23:31:36 -0500

    Added CodingTask06

--- src/GroupHW/CodingTask05.java
+++ src/GroupHW/CodingTask05.java
@@ -18,14 +18,14 @@ public class CodingTask05 {
 		int x = 38;
 		int y = 18;
 
-		System.out.println("Before swapping a = " + x + " and b = " + y );
+		System.out.println("Before swapping a = " + x + " and b = " + y);
 
-		x = x + y; //a = 38 + 18 = 56
-		y = x - y; //b = 56 - 18 = 38
-		x = x - y; //a = 56 - 38 = 18
+		x = x + y; // a = 38 + 18 = 56
+		y = x - y; // b = 56 - 18 = 38
+		x = x - y; // a = 56 - 38 = 18
 
 		System.out.println("After swapping a = " + x + " and b = " + y);
-		
+
 	}
 
 }
---
+++ src/GroupHW/CodingTask06.java
@@ -0,0 +1,36 @@
+package GroupHW;
+
+import java.util.Scanner;
+
+public class CodingTask06 {
+	
+	public static void main(String[] args) {
+		
+		// Write a java program to check whether a given number is prime or not?
+		
+		Scanner s = new Scanner(System.in);
+		System.out.print("Enter a number : ");
+		
+		int n = s.nextInt();
+		
+		if (isPrime(n)) {
+			System.out.println(n + " is a prime number");
+		} else {
+			System.out.println(n + " is not a prime number");
+		}
+	}
+
+	public static boolean isPrime(int n) {
+		if (n <= 1) {
+			return false;
+		}
+		for (int i = 2; i < Math.sqrt(n); i++) {
+			if (n % i == 0) {
+				return false;
+			}
+		}
+		return true;
+
+	}
+
+}
